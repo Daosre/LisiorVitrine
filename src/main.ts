464c0bef4ac1393a7740ba86bcb8728268e87434
Daorse <mmorpgplays5@gmail.com>
2024-12-03 21:24:24 +0100

    Initialisation Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Test API')
+    .setDescription('Voici ma première API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
