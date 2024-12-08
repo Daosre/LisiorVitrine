65a08918d5a0d32c81d6563091e75ee452a0cd81
Daorse <mmorpgplays5@gmail.com>
2024-12-08 22:08:34 +0100

    Inscription plus connexion vaidé

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto, SignUpDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/SignUp')
+  signUp(@Body() dto: SignUpDto) {
+    return this.authService.signUp(dto);
+  }
+
+  @Post('/SignIn')
+  signIp(@Body() dto: SignInDto) {
+    return this.authService.signIn(dto);
+  }
 }
